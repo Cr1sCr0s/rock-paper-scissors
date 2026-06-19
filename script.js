7065ec626671823d431b04028f62930909ead962
cris <crisrelayo17@gmail.com>
2026-06-19 09:28:09 +0800

    Add a function that prompts user for a choice

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ function getComputerChoice(){
   const randNum = Math.floor(Math.random() * 10); // 0-9
 
   if(randNum % 3 === 0){
-    return "rock";
+    return 'rock';
   }else if(randNum % 3 === 1){
-    return "paper";
+    return 'paper';
   }else if(randNum % 3 === 2){
-    return "scissors";
+    return 'scissors';
   }
 }
 console.log(getComputerChoice());
+
+function getHumanChoice(){
+  const choice = prompt('Enter choice:', 'rock');
+  return choice;
+}
+console.log(getHumanChoice());
