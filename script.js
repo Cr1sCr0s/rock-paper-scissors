857bdde85d082409c1ac4b6431f1c9f2a33d6237
cris <crisrelayo17@gmail.com>
2026-06-29 09:51:59 +0800

    Rework logic to use selector buttons
    The idea is to interact with the page itself as well
    as see the results in the same place. Scores will be
    tracked in global variables. The playRound() function
    now utilizes an event from the button's even listeners.
    And instead of the console having the output, text will
    be stored in a paragraph and added to a div in the html
    in the end. Another function will also determine the
    results for the current round. If the player or computer
    reaches a score of 5, they win and the scores reset.

--- script.js
+++ script.js
@@ -1,57 +1,41 @@
-playGame(); // function call, starts game
+const btnRock = document.querySelector('#rock');
+const btnPaper = document.querySelector('#paper');
+const btnScissors = document.querySelector('#scissors');
+btnRock.addEventListener('click', playRound);
+btnPaper.addEventListener('click', playRound);
+btnScissors.addEventListener('click', playRound);
 
-function playGame(){
-  let humanScore = 0;
-  let computerScore = 0;
-
-  // plays 5 rounds
-  for(let i=0; i<5; i++){
-    const winner = playRound(getHumanChoice(), getComputerChoice());
-    // increments score of the winner
-    switch(winner){
-      case 'human':
-        humanScore++;
-        break;
-      case 'computer':
-        computerScore++;
-        break;
-      default:
-        break;
-    }
-    // prints updates scores
-    console.log(`Player Score: ${humanScore}\nComputer Score: ${computerScore}`);
-  }
-
-  // prints results of the game
-  console.log('GAME OVER');
-  if(humanScore > computerScore){
-    console.log('WINNER: human');
-  }else if(humanScore < computerScore){
-    console.log('WINNER: computer');
-  }else{
-    console.log('WINNER: none');
-  }
-}
+let humanScore = 0;
+let computerScore = 0;
 
 // plays one round
-function playRound(humanChoice, computerChoice){
-  humanChoice.toLowerCase();
-  console.log(`The computer chose ${computerChoice}`);
+function playRound(e){
+  const humanChoice = e.target.value;
+  const computerChoice = getComputerChoice();
+  const results = document.querySelector('.results');
+  const output = document.createElement('p');
+
+  output.appendChild(
+    document.createTextNode(
+    `The computer chose ${computerChoice}`
+    )
+  );
 
   let winner = 'none';
+  let outcome = '';
 
   switch(humanChoice){
     case 'rock':
       switch(computerChoice){
         case 'rock':
-          console.log('Tie!');
+          outcome = 'Tie!';
           break;
         case 'paper':
-          console.log('You lose! Paper beats rock.');
+          outcome = 'You lose! Paper beats rock.';
           winner = 'computer';
           break;
         case 'scissors':
-          console.log('You won! Rock beats scissors.');
+          outcome = 'You won! Rock beats scissors.';
           winner = 'human';
           break;
       }
@@ -60,14 +44,14 @@ function playRound(humanChoice, computerChoice){
     case 'paper':
       switch(computerChoice){
         case 'rock':
-          console.log('You won! Paper beats rock.');
+          outcome = 'You won! Paper beats rock.';
           winner = 'human';
           break;
         case 'paper':
-          console.log('Tie!');
+          outcome = 'Tie!';
           break;
         case 'scissors':
-          console.log('You lose! Scissors beats paper.');
+          outcome = 'You lose! Scissors beats paper.';
           winner = 'computer';
           break;
       }
@@ -76,21 +60,102 @@ function playRound(humanChoice, computerChoice){
     case 'scissors':
       switch(computerChoice){
         case 'rock':
-          console.log('You lose! Rock beats paper.');
+          outcome = 'You lose! Rock beats paper.';
           winner = 'computer';
           break;
         case 'paper':
-          console.log('You won! Scissors beats paper.');
+          outcome = 'You won! Scissors beats paper.';
           winner = 'human';
           break;
         case 'scissors':
-          console.log('Tie!');
+          outcome = 'Tie!';
           break;
       }
       break;
   }
 
-  return winner;
+  output.appendChild(document.createElement('br'));
+  output.appendChild(
+    document.createTextNode(outcome)
+  );
+
+  if(winner === 'none'){
+    results.appendChild(output);
+    return;
+  }
+
+  output.appendChild(document.createElement('br'));
+  output.appendChild(
+    document.createTextNode(winner)
+  );
+
+  const status = scoresTrack(winner, results, output);
+
+  return;
+}
+
+function scoresTrack(winner, results, output){
+  // increments score of the winner
+  switch(winner){
+    case 'human':
+      humanScore++;
+      break;
+    case 'computer':
+      computerScore++;
+      break;
+    default:
+      break;
+  }
+  // prints updates scores
+  output.appendChild(document.createElement('br'));
+  output.appendChild(
+    document.createTextNode(
+      `Player Score: ${humanScore}`
+    )
+  );
+  output.appendChild(document.createElement('br'));
+  output.appendChild(
+    document.createTextNode(
+      `Computer Score: ${computerScore}`
+    )
+  );
+
+  if(humanScore === 5 || computerScore === 5){
+    // prints results of the game
+    output.appendChild(document.createElement('br'));
+    output.appendChild(document.createElement('br'));
+    output.appendChild(
+      document.createTextNode('GAME OVER')
+    );
+    if(humanScore > computerScore){
+      output.appendChild(document.createElement('br'));
+      output.appendChild(
+        document.createTextNode(
+          'WINNER: human'
+        )
+      );
+    }else if(humanScore < computerScore){
+      output.appendChild(document.createElement('br'));
+      output.appendChild(
+        document.createTextNode(
+          'WINNER: computer'
+        )
+      );
+    }else{
+      output.appendChild(document.createElement('br'));
+      output.appendChild(
+        document.createTextNode(
+          'WINNER: none'
+        )
+      );
+    }
+
+    humanScore = 0;
+    computerScore = 0;
+  }
+
+  results.appendChild(output);
+  return;
 }
 
 function getComputerChoice(){
@@ -106,9 +171,4 @@ function getComputerChoice(){
   }else if(randNum % 3 === 2){
     return 'scissors';
   }
-}
-
-function getHumanChoice(){
-  const choice = prompt('Enter choice:', 'rock');
-  return choice;
 }
